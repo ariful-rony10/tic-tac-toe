850f037d37a0691b7f08e71d4110213bca380717
Md. Ariful Islam <ariful.rony10@gmail.com>
2022-02-16 23:14:31 +0600

    player name form validation complete

--- script/app.js
+++ script/app.js
@@ -1,9 +1,14 @@
-const playerConfigOverlayElement = document.querySelector('#config-overlay')
-const playerConfigOverlayElement = document.querySelector('#backdrop')
+const playerConfigOverlayElement = document.querySelector('#config-overlay');
+const backdropElement = document.querySelector('#backdrop');
+const formElement = document.querySelector('form');
+const errorOutputElement = document.querySelector('#config-error');
 
 const editPlayer1BtnElement = document.querySelector('#edit-player-1-btn');
 const editPlayer2BtnElement = document.querySelector('#edit-player-2-btn');
-
+const cancelConfigBtnElement = document.querySelector('#cancel-config-button')
 
 editPlayer1BtnElement.addEventListener('click', openPlayerConfig)
 editPlayer2BtnElement.addEventListener('click', openPlayerConfig)
+cancelConfigBtnElement.addEventListener('click', closePlayerConfig)
+backdropElement.addEventListener('click', closePlayerConfig)
+formElement.addEventListener('submit', savePlayerConfig)
--- script/config.js
+++ script/config.js
@@ -1,3 +1,25 @@
 function openPlayerConfig() {
+    playerConfigOverlayElement.style.display = 'block';
+    backdropElement.style.display = 'block'
+}
+
+function closePlayerConfig() {
+    playerConfigOverlayElement.style.display = 'none';
+    backdropElement.style.display = 'none'
+    formElement.firstElementChild.classList.remove('error');
+    errorOutputElement.textContent = '';
+
+}
+
+function savePlayerConfig(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    // Validating value 
+    const enteredPlayerName = formData.get('playername').trim();
+    if (!enteredPlayerName) {
+        event.target.firstElementChild.classList.add('error');
+        errorOutputElement.textContent = 'Please enter a valid name!'
+        return;
+    }
     
 }
