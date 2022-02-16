087f271aa0b6c740566ee367da07d181e482604c
Md. Ariful Islam <ariful.rony10@gmail.com>
2022-02-16 22:11:59 +0600

    completed the designing of tic tac toe

---
+++ README.md
@@ -0,0 +1,6 @@
+# Project Features
+=> Configure Player Names
+=> Re-Start The Game
+=> Turn Based Gameplay
+=> Select Fields & Check for Winner
+=> Show "Game Over" Windows
---
+++ script/app.js
@@ -0,0 +1,9 @@
+const playerConfigOverlayElement = document.querySelector('#config-overlay')
+const playerConfigOverlayElement = document.querySelector('#backdrop')
+
+const editPlayer1BtnElement = document.querySelector('#edit-player-1-btn');
+const editPlayer2BtnElement = document.querySelector('#edit-player-2-btn');
+
+
+editPlayer1BtnElement.addEventListener('click', openPlayerConfig)
+editPlayer2BtnElement.addEventListener('click', openPlayerConfig)
